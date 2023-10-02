feb2f72aa7f21dcd20dff09b26e258b7cabc351e
Aminenur Göynük <amine.gynk@gmail.com>
2023-10-02 21:21:28 +0300

    JavaScript
    Spread, Rest, Destructure

--- Spread, Rest, Destructure/Q3.js
+++ Spread, Rest, Destructure/Q3.js
@@ -54,3 +54,53 @@ const {
   scores: [htmlScore, cssScore, jsScore, reactScore],
 } = student;
 console.log(name, htmlScore, cssScore, jsScore, reactScore);
+
+// 8-) convertArrayToObject
+function convertArrayToObject(students) {
+  return students.map((student) => {
+    const [name, skills, scores] = student;
+    return {
+      name,
+      skills,
+      scores,
+    };
+  });
+}
+
+const students = [
+  ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]],
+  ["John", ["HTM", "CSS", "JS", "React"], [85, 80, 85, 80]],
+];
+
+console.log(convertArrayToObject(students));
+
+// 9-)
+
+console.log("9-)");
+
+const student1 = {
+  name: "David",
+  age: 25,
+  skills: {
+    frontEnd: [
+      { skill: "HTML", level: 10 },
+      { skill: "CSS", level: 8 },
+      { skill: "JS", level: 8 },
+      { skill: "React", level: 9 },
+    ],
+    backEnd: [
+      { skill: "Node", level: 7 },
+      { skill: "GraphQL", level: 8 },
+    ],
+    dataBase: [{ skill: "MongoDB", level: 7.5 }],
+    dataScience: ["Python", "R", "D3.js"],
+  },
+};
+
+const newStudent = { ...student1 };
+newStudent.skills.frontEnd.push({ skill: "BootStrap", level: 8 });
+newStudent.skills.backEnd.push({ skill: "Express", level: 9 });
+newStudent.skills.dataBase.push({ skill: "SQL", level: 8 });
+newStudent.skills.dataScience.push("SQL");
+
+console.log(newStudent);
